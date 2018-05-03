39530a0c2b6088639df79003b64f49fc6980665b
Taylor Huang <tahuan@microsoft.com>
2018-05-03 18:14:32 +0800

    add async logic
    增加了异步编程(BackgroundWorker)，防止在刷新时由于等待网络响应产生界面夹死

--- Form1.cs
+++ Form1.cs
@@ -16,24 +16,55 @@ namespace GoFund
     public partial class Form1 : Form
     {
 
-        HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
-
-        
-
+        HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();   
         List<Fund> myList = new List<Fund>();
         string sPage = "http://fund.eastmoney.com/";
         string line = null;
         string[] subline = null;
         string sContent = null;
+        private BackgroundWorker bgWorker = null;        
+
+        private void DoQuery(object sender, DoWorkEventArgs e)
+        {
+            web.CacheOnly = false;
+            web.CachePath = null;
+            web.UsingCache = false;
+            StringBuilder sb = new StringBuilder();
+
+            //获取估值
+            foreach (Fund objFund in myList)
+            {
+
+                HtmlAgilityPack.HtmlDocument doc = web.Load(sPage + objFund.Id + ".html"); // HtmlDocument 类，负责操作html文档
+                HtmlNode ranking = doc.DocumentNode.SelectSingleNode("//div[@class='Rdata']"); // 近一周同类排名
+
+
+                var rate = doc.GetElementbyId("gz_gszzl").InnerText;
+                if (rate.Contains("+") == true)
+                {
+                    rate = "<font color=\"red\">" + rate + "</font>";
+                }
+
+                var date = doc.GetElementbyId("gz_gztime").InnerHtml;
+                if (rate == "--") rate = "0.00%";
+                if (date == "--") date = "暂无";
+                sb.Append(objFund.Id + " " + "<a href=\"" + sPage + objFund.Id + ".html\" target=\"_blank\">" + objFund.Name + "</a>" + "\t" + "估算涨幅：" + rate + "\t" + " 更新时间：" + date + "\t" + "近一周排名：" + ranking.InnerText + objFund.Has + "<br>");
+            }
+            sContent = sb.ToString();
+            if (sContent != null)
+            {
+                webBrowser1.DocumentText = sContent;
+                sContent = null;
+            }
+        }
+
 
         public Form1()
         {
             InitializeComponent();
             timer1.Enabled = true;
 
-            try
-            {
-                //读取基金资料
+            //读取基金资料
                 FileStream fs = new FileStream(@"D:\CodeRepo\GoFund\GoFund\bin\Release\AllFunds.txt", FileMode.Open);
                 StreamReader sr = new StreamReader(fs);
 
@@ -48,64 +79,36 @@ namespace GoFund
                 sr.Close();
                 fs.Close();
 
-            }
-            catch (Exception ex)
+            if(myList.Count !=0)
             {
-
-                MessageBox.Show(ex.Message);
-                throw;
+                bgWorker = new BackgroundWorker();
+                bgWorker.DoWork += new DoWorkEventHandler(DoQuery);
             }
+
         }
 
+        
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                web.CacheOnly = false;
-                web.CachePath = null;
-                web.UsingCache = false;
+                webBrowser1.DocumentText = "资料爬取中...";
+                bgWorker.RunWorkerAsync();
                 
-
-                //获取估值
-                foreach (Fund objFund in myList)
-                {
-
-                    HtmlAgilityPack.HtmlDocument doc = web.Load(sPage + objFund.Id + ".html"); // HtmlDocument 类，负责操作html文档
-                    HtmlNode ranking = doc.DocumentNode.SelectSingleNode("//div[@class='Rdata']"); // 近一周同类排名
-                    
-                    
-                    var rate = doc.GetElementbyId("gz_gszzl").InnerText;
-                    if (rate.Contains("+") == true)
-                    {
-                        rate = "<font color=\"red\">" + rate + "</font>";
-                    }                                          
-
-                    var date = doc.GetElementbyId("gz_gztime").InnerHtml;
-                    if (rate == "--")  rate = "0.00%";
-                    if (date == "--") date = "暂无";
-                    sContent = sContent + objFund.Id + " " + "<a href=\"" + sPage + objFund.Id + ".html\" target=\"_blank\">" + objFund.Name + "</a>" + "\t" + "估算涨幅：" + rate + "\t" + " 更新时间：" + date + "\t"  + "近一周排名："+ ranking.InnerText + objFund.Has + "<br>";
-
-                }
             }
             catch (Exception ex)
             {
 
                 MessageBox.Show(ex.Message);
                 throw;
-            }
-            finally
-            {
-                webBrowser1.DocumentText = sContent;
-                sContent = null;
-            }                        
-            
+            }           
 
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
-            button1_Click(null, null);
+
+            button1_Click(null, null);                   
         }
     }
 }
