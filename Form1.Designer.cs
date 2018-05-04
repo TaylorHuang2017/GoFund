967ff7006b1399f94dfaa7f776329b75ce2116ce
Taylor Huang <tahuan@microsoft.com>
2018-05-04 08:56:07 +0800

    add async logic
    disable the button after the user clicks the refresh
    adds exceptions handling

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // button1
@@ -59,6 +60,11 @@
             this.webBrowser1.Size = new System.Drawing.Size(802, 314);
             this.webBrowser1.TabIndex = 2;
             // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -78,6 +84,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -22,42 +22,6 @@ namespace GoFund
         string line = null;
         string[] subline = null;
         string sContent = null;
-        private BackgroundWorker bgWorker = null;        
-
-        private void DoQuery(object sender, DoWorkEventArgs e)
-        {
-            web.CacheOnly = false;
-            web.CachePath = null;
-            web.UsingCache = false;
-            StringBuilder sb = new StringBuilder();
-
-            //获取估值
-            foreach (Fund objFund in myList)
-            {
-
-                HtmlAgilityPack.HtmlDocument doc = web.Load(sPage + objFund.Id + ".html"); // HtmlDocument 类，负责操作html文档
-                HtmlNode ranking = doc.DocumentNode.SelectSingleNode("//div[@class='Rdata']"); // 近一周同类排名
-
-
-                var rate = doc.GetElementbyId("gz_gszzl").InnerText;
-                if (rate.Contains("+") == true)
-                {
-                    rate = "<font color=\"red\">" + rate + "</font>";
-                }
-
-                var date = doc.GetElementbyId("gz_gztime").InnerHtml;
-                if (rate == "--") rate = "0.00%";
-                if (date == "--") date = "暂无";
-                sb.Append(objFund.Id + " " + "<a href=\"" + sPage + objFund.Id + ".html\" target=\"_blank\">" + objFund.Name + "</a>" + "\t" + "估算涨幅：" + rate + "\t" + " 更新时间：" + date + "\t" + "近一周排名：" + ranking.InnerText + objFund.Has + "<br>");
-            }
-            sContent = sb.ToString();
-            if (sContent != null)
-            {
-                webBrowser1.DocumentText = sContent;
-                sContent = null;
-            }
-        }
-
 
         public Form1()
         {
@@ -78,37 +42,62 @@ namespace GoFund
 
                 sr.Close();
                 fs.Close();
-
-            if(myList.Count !=0)
-            {
-                bgWorker = new BackgroundWorker();
-                bgWorker.DoWork += new DoWorkEventHandler(DoQuery);
-            }
-
         }
 
-        
+    
         private void button1_Click(object sender, EventArgs e)
         {
-            try
-            {
-                webBrowser1.DocumentText = "资料爬取中...";
-                bgWorker.RunWorkerAsync();
-                
-            }
-            catch (Exception ex)
-            {
-
-                MessageBox.Show(ex.Message);
-                throw;
-            }           
-
+            webBrowser1.DocumentText = "资料爬取中...";
+            button1.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
             button1_Click(null, null);                   
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            web.CacheOnly = false;
+            web.CachePath = null;
+            web.UsingCache = false;
+            StringBuilder sb = new StringBuilder();
+            HtmlAgilityPack.HtmlDocument doc = null;
+
+            //获取估值
+            foreach (Fund objFund in myList)
+            {
+
+                try
+                {
+                    doc = web.Load(sPage + objFund.Id + ".html"); // HtmlDocument 类，负责操作html文档
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Network Errors: " + ex.Message);
+                }
+                
+                HtmlNode ranking = doc.DocumentNode.SelectSingleNode("//div[@class='Rdata']"); // 近一周同类排名
+                var rate = doc.GetElementbyId("gz_gszzl").InnerText;
+                if (rate.Contains("+") == true)
+                {
+                    rate = "<font color=\"red\">" + rate + "</font>";
+                }
+
+                var date = doc.GetElementbyId("gz_gztime").InnerHtml;
+                if (rate == "--") rate = "0.00%";
+                if (date == "--") date = "暂无";
+                sb.Append(objFund.Id + " " + "<a href=\"" + sPage + objFund.Id + ".html\" target=\"_blank\">" + objFund.Name + "</a>" + "\t" + "估算涨幅：" + rate + "\t" + " 更新时间：" + date + "\t" + "近一周排名：" + ranking.InnerText + objFund.Has + "<br>");
+            }
+            sContent = sb.ToString();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            webBrowser1.DocumentText = sContent;
+            button1.Enabled = true;
+            sContent = null;
         }
     }
 }
